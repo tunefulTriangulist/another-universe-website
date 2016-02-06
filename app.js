f4f48abf8ba6bef2e0eac8806d89d25e59ecccce
Tina Zhang <tinazhangnz@gmail.com>
2016-02-06 20:49:07 +1300

    Removed the server error debugging code.

--- app.js
+++ app.js
@@ -65,13 +65,13 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    /*res.render('error', {
+    res.render('error', {
     	layout: null,
     	statusCode: res.status,
         message: err.message,
         error: {}
-    });*/
-	res.send(err.status, err.stack);
+    });
+	//res.send(err.status, err.stack);
 });
 
 
