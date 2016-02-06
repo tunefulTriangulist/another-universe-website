fa0abce0455104a1bb492153e773c47ccc485897
Tina Zhang <tinazhangnz@gmail.com>
2016-02-07 00:58:23 +1300

    Changed jQuery triggering again so only images animate after they load; other things animate as soon as document is ready.

--- public/javascripts/contentjquery.js
+++ public/javascripts/contentjquery.js
@@ -1,6 +1,9 @@
-$(window).load(function(){
-	$("#header").animate({"opacity": 1}, 1500);
+$(document).ready(function(){
 	$("#headertext").animate({"opacity": 1}, 300);
 	$("#subtitle").animate({"opacity": 1}, 1000);
 	$("#content").animate({"opacity":1}, 500);
+});
+
+$(window).load(function() {
+	$("#header").animate({"opacity": 1}, 1500);
 });
--- public/javascripts/homejquery.js
+++ public/javascripts/homejquery.js
@@ -1,7 +1,10 @@
-$(window).load(function() {
+$(document).ready(function() {
 	$("#headertext").animate({"opacity":1}, 800);
 	$("#subtitle").animate({"opacity":1}, 800);
 	$("#button").animate({"top":"370px"}, 1500);
-	$("#header").animate({"opacity":1}, 1500);
 	$("#content").animate({"opacity":1}, 800);
+});
+
+$(window).load(function() {
+	$("#header").animate({"opacity":1}, 1500);
 });
