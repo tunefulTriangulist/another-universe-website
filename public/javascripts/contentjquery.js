117312c3eb8b3b5fcbd186057ea755de8ca6201d
Tina Zhang <tinazhangnz@gmail.com>
2016-02-06 21:59:50 +1300

    Changed jQuery animations (speeds and styles).

--- public/javascripts/contentjquery.js
+++ public/javascripts/contentjquery.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-	$("#header").animate({"opacity": 1});
-	$("#headertext").slideDown("slow");
-	$("#subtitle").animate({"opacity": 1});
+	$("#header").animate({"opacity": 1}, 1500);
+	$("#headertext").animate({"opacity": 1}, 300);
+	$("#subtitle").animate({"opacity": 1}, 1000);
 	$("#content").animate({"opacity":1}, 500);
 });
--- public/javascripts/homejquery.js
+++ public/javascripts/homejquery.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-	$("#headertext").slideDown("slow");
-	$("#subtitle").slideDown("slow");
-	$("#button").animate({"top":"370px"}, 500);
-	$("#header").animate({"opacity":1}, 500);
-	$("#content").animate({"opacity":1}, 500);
+	$("#headertext").animate({"opacity":1}, 800);
+	$("#subtitle").animate({"opacity":1}, 800);
+	$("#button").animate({"top":"370px"}, 1500);
+	$("#header").animate({"opacity":1}, 1500);
+	$("#content").animate({"opacity":1}, 800);
 });
